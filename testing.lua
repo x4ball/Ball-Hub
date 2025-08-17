30d6d4b8642354cc38be2c4bb344a338163023d0
x4ball <fayrixt@gmail.com>
2025-08-18 04:31:12 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -8,11 +8,19 @@ local Window = WindUI:CreateWindow({
     Size = UDim2.fromOffset(600, 460),
     Theme = "Dark",
 })
+Window:Tag({
+    Title = "Working",
+    Color = Color3.fromHex("#30ff6a")
+})
+Window:Tag({
+    Title = "Beta Version",
+    Color = Color3.fromHex("#30ff6a")
+})
 
 -- 🔹 HOME TAB
 local Home = Window:Tab({
     Title = "Home",
-    Icon = "home",
+    Icon = "house",
 })
 
 local HomeSection = Home:Section({
@@ -52,6 +60,17 @@ ChangelogSection:Paragraph({
     Title = "Perubahan",
     Desc = "- Fix bug teleport\n- Tambah Dealer Toyota\n- UI Home Profile baru",
     Color = "Green",
+})
+-- 🔹 PLAYER TAB
+local PlayerTab = Window:Tab({
+    Title = "Player",
+    Icon = "player",
+})
+
+local PlayerSection = PlayerTab:Section({ 
+    Title = "Feature",
+    TextXAlignment = "Left",
+    TextSize = 20, 
 })
 
 -- 🔹 DEALERSHIP TAB
@@ -99,5 +118,163 @@ DealerSection:Dropdown({
                 Icon = "x",
             })
         end
+    end
+})
+
+local DealerSection = DealerTab:Section({
+    Title = "Dealer Jawa barat"
+})
+
+local Places = {
+    ["Dealer Mitshubisi JWB"] = Vector3.new(2827.5, 29, 100),
+    ["Dealer Mercedes JWB"] = Vector3.new(100, 10, 50),
+    ["Dealer Toyota JWB"] = Vector3.new(0, 5, 0),
+}
+
+DealerSection:Dropdown({
+    Title = "Pilih Dealer",
+    Values = {"Dealer Mitshubisi JKT", "Dealer Mercedes JKT", "Dealer Toyota JKT"},
+    Value = "Dealer Mitshubisi JKT",
+    Callback = function(option)
+        local pos = Places[option]
+        if pos then
+            WindUI:Notify({
+                Title = "Teleport",
+                Content = "Teleporting ke " .. option .. "...",
+                Duration = 2,
+                Icon = "loader",
+            })
+            task.wait(1)
+            player.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
+            WindUI:Notify({
+                Title = "Sukses ✅",
+                Content = "Berhasil teleport ke " .. option,
+                Duration = 3,
+                Icon = "check",
+            })
+        else
+            WindUI:Notify({
+                Title = "Gagal ❌",
+                Content = "Lokasi dealer nggak ditemukan",
+                Duration = 3,
+                Icon = "x",
+            })
+        end
+    end
+})
+
+local DealerSection = DealerTab:Section({
+    Title = "Dealer Jawa Tengah"
+})
+
+local Places = {
+    ["Dealer Mitshubisi JWB"] = Vector3.new(2827.5, 29, 100),
+    ["Dealer Mercedes JWB"] = Vector3.new(100, 10, 50),
+    ["Dealer Toyota JWB"] = Vector3.new(0, 5, 0),
+}
+
+DealerSection:Dropdown({
+    Title = "Pilih Dealer",
+    Values = {"Dealer Mitshubisi JKT", "Dealer Mercedes JKT", "Dealer Toyota JKT"},
+    Value = "Dealer Mitshubisi JKT",
+    Callback = function(option)
+        local pos = Places[option]
+        if pos then
+            WindUI:Notify({
+                Title = "Teleport",
+                Content = "Teleporting ke " .. option .. "...",
+                Duration = 2,
+                Icon = "loader",
+            })
+            task.wait(1)
+            player.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
+            WindUI:Notify({
+                Title = "Sukses ✅",
+                Content = "Berhasil teleport ke " .. option,
+                Duration = 3,
+                Icon = "check",
+            })
+        else
+            WindUI:Notify({
+                Title = "Gagal ❌",
+                Content = "Lokasi dealer nggak ditemukan",
+                Duration = 3,
+                Icon = "x",
+            })
+        end
+    end
+})
+
+local DealerSection = DealerTab:Section({
+    Title = "Dealer Jawa Tengah"
+})
+
+local Places = {
+    ["Dealer Mitshubisi JWB"] = Vector3.new(2827.5, 29, 100),
+    ["Dealer Mercedes JWB"] = Vector3.new(100, 10, 50),
+    ["Dealer Toyota JWB"] = Vector3.new(0, 5, 0),
+}
+
+DealerSection:Dropdown({
+    Title = "Pilih Dealer",
+    Values = {"Dealer Mitshubisi JKT", "Dealer Mercedes JKT", "Dealer Toyota JKT"},
+    Value = "Dealer Mitshubisi JKT",
+    Callback = function(option)
+        local pos = Places[option]
+        if pos then
+            WindUI:Notify({
+                Title = "Teleport",
+                Content = "Teleporting ke " .. option .. "...",
+                Duration = 2,
+                Icon = "loader",
+            })
+            task.wait(1)
+            player.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
+            WindUI:Notify({
+                Title = "Sukses ✅",
+                Content = "Berhasil teleport ke " .. option,
+                Duration = 3,
+                Icon = "check",
+            })
+        else
+            WindUI:Notify({
+                Title = "Gagal ❌",
+                Content = "Lokasi dealer nggak ditemukan",
+                Duration = 3,
+                Icon = "x",
+            })
+        end
+    end
+})
+
+-- 🔹 JOB APPLICATION TAB
+local Job = Window:Tab({
+    Title = "Job Application",
+    Icon = "job",
+})
+
+local JobSection = Job:Section({
+    Title = "Job Place in Jakarta"
+})
+
+local JobButton = Tab:Button({
+    Title = "Teleport To Office",
+    Desc = "Teleport to office job buliding.",
+    Locked = false,
+    Callback = function()
+        WindUI:Notify({
+                Title = "Teleport",
+                Content = "Teleport To Office place",
+                Duration = 2,
+                Icon = "loader",
+            })
+        task.wait(1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new()
+        WindUI:Notify({
+                Title = "Sukses",
+                Content = "Berhasil teleport ke ",
+                Duration = 3,
+                Icon = "check",
+        })
     end
 })
