84e42e246157124f7eb71a9e736d417276e87222
x4ball <fayrixt@gmail.com>
2025-08-18 08:13:40 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -68,7 +68,7 @@ mtb1:Dropdown({
                 Icon = "loader",
             })
             task.wait(1)
-            Player.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
             WindUI:Notify({
                 Title = "Sukses ✅",
                 Content = "Berhasil teleport ke " .. option,
